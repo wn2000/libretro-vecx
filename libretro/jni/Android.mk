1a9595e31ac4e02e2455e43ec19b67008ea01123
twinaphex <libretro@gmail.com>
2014-11-01 08:02:37 +0100

    Add Makefile.common - reuse this in Android.mk file

---
+++ libretro/jni/Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+CORE_DIR     := ../..
+LIBRETRO_DIR := ..
+
+LOCAL_MODULE    := retro
+
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_CFLAGS += -DANDROID_ARM
+LOCAL_ARM_MODE := arm
+endif
+
+ifeq ($(TARGET_ARCH),x86)
+LOCAL_CFLAGS +=  -DANDROID_X86
+endif
+
+ifeq ($(TARGET_ARCH),mips)
+LOCAL_CFLAGS += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
+endif
+
+include ../../Makefile.common
+
+LOCAL_SRC_FILES    += $(SOURCES_C)
+LOCAL_CFLAGS += -O2 -std=gnu99 -ffast-math -DINLINE=inline -DHAVE_STRINGS_H -DHAVE_STDINT_H -DHAVE_INTTYPES_H -D__LIBRETRO__ -DRIGHTSHIFT_IS_SAR -DFRONTEND_SUPPORTS_RGB565 -DNDEBUG=1 $(INCFLAGS)
+
+include $(BUILD_SHARED_LIBRARY)
