896faf139564986bc50bb91a22cfd5bf223aab40
Gregor Richards <hg-yff@gregor.im>
2016-12-09 18:05:20 -0500

    Report git version with library_version

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -59,7 +59,10 @@ void retro_get_system_info(struct retro_system_info *info)
 {
 	memset(info, 0, sizeof(*info));
 	info->library_name = "VecX";
-	info->library_version = "1.2";
+#ifndef GIT_VERSION
+#define GIT_VERSION ""
+#endif
+	info->library_version = "1.2" GIT_VERSION;
 	info->need_fullpath = false;
 	info->valid_extensions = "bin|vec";
 }
